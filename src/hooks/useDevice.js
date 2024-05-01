5ec6a00a1eafe6e94dfb652864411dd46a4ca4e1
Jay <Jay@geekstudio.co.kr>
2024-05-01 13:44:07 +0900

    :sparkles: feat: useDevice hooks 추가

---
+++ src/hooks/useDevice.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+
+export const Mobile = ({ children }) => {
+    const isMobile = useMediaQuery({
+        query: "(max-width:767px)"
+    });
+    return <>{isMobile && children}</>
+}
+
+export const Tablet = ({ children }) => {
+    const isTablet = useMediaQuery({
+        query: "(min-width:768px) and (max-width:1159px)"
+    });
+    return <>{isTablet && children}</>
+}
+
+export const Pc = ({ children }) => {
+    const isPc = useMediaQuery({
+        query: "(min-width:1160px)"
+    });
+    return <>{isPc && children}</>
+}
