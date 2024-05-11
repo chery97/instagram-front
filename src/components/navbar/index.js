2f0e9faf93ba54d8649b43f547c215e74aa40e78
Jay <Jay@geekstudio.co.kr>
2024-05-12 00:46:16 +0900

    :hammer: add: 검색창 추가

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {mediaQueries, Mobile, Pc, Tablet} from '../../hooks/useDevice';
-import styled from "styled-components";
+import styled, {css, keyframes} from "styled-components";
 import Logo from "../../asset/icons/nav-bar-logo.svg";
 import Logo1 from "../../asset/icons/nav-bar-logo-1.svg";
 import Home from "../../asset/icons/nav-bar-home.svg";
@@ -18,6 +18,7 @@ const NavbarContainer = styled.div`
   width: 244px;
   height: 100vh;
   display: flex;
+  position: relative;
   background-color: black;
   ${mediaQueries.mobile} {
     font-size: 16px;
@@ -51,12 +52,6 @@ const LogoInner = styled(Link)`
   margin: 0 0 19px;
   display: flex;
   img {
-    ${mediaQueries.desktop} {
-      content: url(${Logo});
-    }
-    ${mediaQueries.tablet} {
-      content: url(${Logo1});
-    }
     filter: invert(100%);
   }
 `;
@@ -125,12 +120,123 @@ const SubLayout = styled.div`
   width: 397px;
   height: 100vh;
   bottom: 0;
-  left: 153px;
+  left: 80px;
   position: absolute;
   background-color: black;
+  animation: ${() => slideInAnimation} 0.3s ease-in-out forwards;
   img, span {
     filter: invert(100%);
   }
+`;
+
+const slideInAnimation = keyframes`
+  from {
+    transform: translateX(-20%);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+const SubLayoutInner = styled.div`
+  padding: 8px 0;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SubLayoutTop = styled.div`
+  padding: 12px 14px 36px 24px;
+  margin: 8px 0;
+  div {
+    text-align: left;
+    font-size: 28px;
+    font-weight: 600;
+    max-width: 100%;
+  }
+`;
+
+const SubLayoutBottom = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SubLayoutSearchBox = styled.div`
+  margin: 0 16px;
+  max-width: 100%;
+  input {
+    color: rgb(38, 38, 38);
+  }
+`;
+
+const SubLayoutSearchBoxInner = styled.div`
+  margin-bottom: 24px;
+  height: 40px;
+  min-width: 125px;
+  background-color: #262626;
+  border-radius: 8px;
+  padding: 0 16px;
+  cursor: text;
+`;
+
+const SearchBoxCont = styled.div`
+  color: #A8A8A8;
+  font-size: 16px;
+  display: flex;
+  height: 100%;
+  align-items: center;
+  img {
+    width: 16px;
+  }
+`;
+
+const SearchBoxInputCont = styled.div`
+  color: #A8A8A8;
+  font-size: 16px;
+  display: flex;
+  height: 100%;
+  align-items: center;
+  input {
+    color: #A8A8A8;
+    font-size: 16px;
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const SearchBoxContDeleteBtn = styled.div`
+  background-repeat: no-repeat;
+  background-position: -318px -333px;
+  height: 20px;
+  width: 20px;
+  background-image: url("https://static.cdninstagram.com/rsrc.php/v3/y5/r/TJztmXpWTmS.png");
+`;
+
+const Divide = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: rgb(38, 38, 38);
+`;
+
+const ResultBox = styled.div`
+  width: 100%;
+  height: 100%;
+  padding-top: 12px;
+`;
+
+const ResultBoxTitle = styled.div`
+  width: 100%;
+  max-width: 348px;
+  display: flex;
+  margin: 6px 24px 8px 24px;
+  padding-top: 4px;
+  color: #F5F5F5;
+  font-size: 16px;
+  font-weight: 800;
 `;
 
 const Navbar = () => {
@@ -158,33 +264,68 @@ const Navbar = () => {
         contents.image = contents.image[id];
     })
 
-    const openSubLayout = (id) => {
-        if (id === "Search") {
-            console.log(id)
-        }
+    const [isSubLayoutVisible,setIsSubLayoutVisible] = useState('');
+    const [isInputType,setIsInputType] = useState(false);
+
+    const element = useRef(null);
+    const useSlideIn = () => {
+        useEffect(() => {
+            const clickFocus = (e) => {
+                // 검색창 요소 이외의 영역 클릭 시 검색창 비노출 되도록 value 빈 값으로 셋팅
+                if (element.current && !element.current.contains(e.target)) {
+                    setIsSubLayoutVisible('');
+                }
+            }
+            // 이벤트 리스너에 clickFocus 함수 등록
+            document.addEventListener("mouseup", clickFocus);
+
+            return () => {
+                // 끝나면 리스너 삭제
+                document.removeEventListener("mouseup", clickFocus);
+            }
+        }, [isSubLayoutVisible]);
+        return { ref: element, style: { opacity: 1 } };
     }
+    const openSubLayout = useSlideIn();
 
     return (
         <>
-            <NavbarContainer>
+            <NavbarContainer style={{
+                width: isSubLayoutVisible === 'Search' ? 'fit-content' : null
+            }}>
                 <NavBarLayout>
                     <LogoBox>
                         <LogoInner to={'/'}>
-                            <img src={Logo1} alt=''/>
+                            <Pc>
+                                <img src={
+                                    isSubLayoutVisible !== 'Search' ?
+                                        Logo : Logo1
+                                } alt=''/>
+                            </Pc>
+                            <Tablet>
+                                <img src={
+                                    isSubLayoutVisible !== 'Search' ?
+                                        Logo1 : Logo
+                                } alt=''/>
+                            </Tablet>
                         </LogoInner>
                     </LogoBox>
                     <MiddleBox>
                         {mainContents.map((contents, index) => (
                             <React.Fragment key={index}>
-                                <MiddleInner onClick={() => openSubLayout(contents.id)}>
+                                <MiddleInner onClick={() => setIsSubLayoutVisible(contents.id)}>
                                     <Tablet>
                                         <img src={contents.image}  alt=''/>
                                     </Tablet>
                                     <Pc>
                                         <img src={contents.image}  alt=''/>
-                                        <MiddleText>
-                                            <span>{contents.label}</span>
-                                        </MiddleText>
+                                        {isSubLayoutVisible !== 'Search'
+                                            ?
+                                            <MiddleText>
+                                                <span>{contents.label}</span>
+                                            </MiddleText>
+                                            : null
+                                        }
                                     </Pc>
                                 </MiddleInner>
                             </React.Fragment>
@@ -195,29 +336,57 @@ const Navbar = () => {
                             <React.Fragment key={index}>
                                 <BottomInner>
                                     <Tablet>
-                                        <img src={contents.image}  alt=''/>
+                                        <img src={contents.image} alt=''/>
                                     </Tablet>
                                     <Pc>
-                                        <img src={contents.image}  alt=''/>
-                                        <BottomText>
-                                            <span>{contents.label}</span>
-                                        </BottomText>
+                                        <img src={contents.image} alt=''/>
+                                        {isSubLayoutVisible !== 'Search'
+                                            ?
+                                                <BottomText>
+                                                    <span>{contents.label}</span>
+                                                </BottomText>
+                                            : null
+                                        }
                                     </Pc>
                                 </BottomInner>
                             </React.Fragment>
                         ))}
                     </BottomBox>
                 </NavBarLayout>
-                {/*<SubLayout>
-                    <div>
-                        <div>
-                            <div>
-                                <span>검색</span>
-                            </div>
-                        </div>
-                    </div>
-                </SubLayout>
-                */}
+                {isSubLayoutVisible === 'Search' ? (
+                    <SubLayout {...openSubLayout} ref={ element }>
+                        <SubLayoutInner>
+                            <SubLayoutTop>
+                                <div>
+                                    <span>검색</span>
+                                </div>
+                            </SubLayoutTop>
+                            <SubLayoutBottom>
+                                <SubLayoutSearchBox>
+                                    <SubLayoutSearchBoxInner>
+                                        {!isInputType
+                                            ?
+                                            <SearchBoxCont onClick={() => setIsInputType(true)}>
+                                                <img src={Search} alt=''/>
+                                                <span>검색</span>
+                                            </SearchBoxCont>
+                                            :
+                                            <SearchBoxInputCont>
+                                                <input type='text' placeholder='검색' />
+                                                <SearchBoxContDeleteBtn />
+                                            </SearchBoxInputCont>
+                                        }
+                                    </SubLayoutSearchBoxInner>
+                                </SubLayoutSearchBox>
+                                <Divide />
+                                <ResultBox>
+                                    <ResultBoxTitle>최근 검색 항목</ResultBoxTitle>
+                                </ResultBox>
+                            </SubLayoutBottom>
+                        </SubLayoutInner>
+                    </SubLayout>
+                    ) : null
+                }
             </NavbarContainer>
         </>
     );
