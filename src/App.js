0a73300a09498ba2e4abbf0c94cc59d5e370742f
Joy <94097424+jiyeon-520@users.noreply.github.com>
2024-05-11 19:27:27 +0900

    :hammer: refactor: 메인 페이지 마크업 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router-dom";
-import { GlobalStyle } from "./pages/style/global-style";
+import { Outlet } from 'react-router-dom';
+import { GlobalStyle } from './pages/style/global-style';
 
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Outlet />
-      <GlobalStyle />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Outlet />
+            <GlobalStyle />
+        </div>
+    );
 }
 
 export default App;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,155 +1,270 @@
-import { MainFeedStyled as S } from "./index.styled";
-import React, { useMemo, useRef, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useMemo } from 'react';
 
-import "swiper/css";
-import "swiper/css/navigation";
+import { MainFeedStyled as S } from './index.styled';
 
-import { Navigation } from "swiper/modules";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper/modules';
+import { isMobile } from '../../utils/mediaQueries';
 
 const MainFeed = () => {
-  const Settings = useMemo(() => {
-    return {
-      modules: [Navigation],
-      slidesPerView: 8,
-      navigation: {
-        prevEl: ".swiper-button-prev-event",
-        nextEl: ".swiper-button-next-event",
-      },
-    };
-  });
+    const isMobileSize = isMobile();
 
-  const SettingsFeed = useMemo(() => {
-    return {
-      modules: [Navigation],
-      slidesPerView: 1,
-      navigation: {
-        prevEl: ".swiper-button-prev-event",
-        nextEl: ".swiper-button-next-event",
-      },
-    };
-  });
+    const Settings = useMemo(() => {
+        return {
+            modules: [Navigation],
+            slidesPerView: 8,
+            spaceBetween: 20,
+            navigation: {
+                prevEl: '.swiper-button-prev-event',
+                nextEl: '.swiper-button-next-event',
+            },
+        };
+    });
 
-  return (
-    <S.Main>
-      <S.Inner>
-        <nav style={{ width: "245px" }}>navBar componenets</nav>
-        <S.Contents>
-          <S.MainContents>
-            <S.StorySection>
-              <Swiper {...Settings}>
-                <S.Slide>
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-                <S.Slide>
-                  {" "}
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-                <S.Slide>
-                  {" "}
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-                <S.Slide>
-                  {" "}
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-                <S.Slide>
-                  {" "}
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-                <S.Slide>
-                  {" "}
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-                <S.Slide>
-                  {" "}
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-                <S.Slide>
-                  {" "}
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-                <S.Slide>
-                  {" "}
-                  <img src="https://picsum.photos/id/237/65/65" alt="" />
-                  <span>Slide 1</span>
-                </S.Slide>
-              </Swiper>
-            </S.StorySection>
-            <S.FeedSection>
-              <S.Feed>
-                <S.Header>
-                  <div>
-                    <S.Profile>
-                      <img src="https://picsum.photos/id/237/65/65" alt="" />
-                    </S.Profile>
-                    <S.Name>lovtaeha</S.Name>
-                    <S.Time>2일</S.Time>
-                  </div>
-                  <S.AddButton>...</S.AddButton>
-                </S.Header>
-                <S.Photo>
-                  <Swiper {...SettingsFeed}>
-                    <S.Slide>
-                      {" "}
-                      <img src="https://picsum.photos/id/237/65/65" alt="" />
-                      <span>Slide 1</span>
-                    </S.Slide>
-                    <S.Slide>
-                      {" "}
-                      <img src="https://picsum.photos/id/237/65/65" alt="" />
-                      <span>Slide 1</span>
-                    </S.Slide>
-                    <S.Slide>
-                      {" "}
-                      <img src="https://picsum.photos/id/237/65/65" alt="" />
-                      <span>Slide 1</span>
-                    </S.Slide>
-                    <S.Slide>
-                      {" "}
-                      <img src="https://picsum.photos/id/237/65/65" alt="" />
-                      <span>Slide 1</span>
-                    </S.Slide>
-                    <S.Slide>
-                      {" "}
-                      <img src="https://picsum.photos/id/237/65/65" alt="" />
-                      <span>Slide 1</span>
-                    </S.Slide>
-                  </Swiper>
-                </S.Photo>
-                <S.ActionIcon>
-                  <div>
-                    <S.Like>좋아요</S.Like>
-                    <S.Reply>댓글</S.Reply>
-                    <S.Share>공유</S.Share>
-                  </div>
-                  <S.BookMark>저장</S.BookMark>
-                </S.ActionIcon>
-                <S.LikePeople>
-                  님 <span>여러 명</span>이 좋아합니다
-                </S.LikePeople>
-                <S.Post>
-                  <S.Name>lovtaeha</S.Name>
-                  <S.Text>
-                    어린이 날 스스로 계획한대로... <span>더보기</span>
-                  </S.Text>
-                </S.Post>
-              </S.Feed>
-            </S.FeedSection>
-          </S.MainContents>
-          <S.Recommend>recommend</S.Recommend>
-        </S.Contents>
-      </S.Inner>
-    </S.Main>
-  );
+    const SettingsFeed = useMemo(() => {
+        return {
+            modules: [Navigation],
+            slidesPerView: 1,
+            navigation: {
+                prevEl: '.swiper-button-prev-event',
+                nextEl: '.swiper-button-next-event',
+            },
+        };
+    });
+
+    return (
+        <S.Main>
+            <S.Inner>
+                {/* <nav style={{ width: '245px' }}>navBar componenets</nav> */}
+                <S.Contents>
+                    <S.MainContents>
+                        <S.StorySection>
+                            <S.SwiperWrap {...Settings}>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                                <S.Slide>
+                                    <S.ImageBox>
+                                        <img
+                                            src='https://picsum.photos/id/237/65/65'
+                                            alt=''
+                                        />
+                                    </S.ImageBox>
+                                    <span>Name</span>
+                                </S.Slide>
+                            </S.SwiperWrap>
+                        </S.StorySection>
+                        <S.FeedSection>
+                            <S.Feed>
+                                <S.Header>
+                                    <div>
+                                        <S.Profile>
+                                            <img
+                                                src='https://picsum.photos/id/237/65/65'
+                                                alt=''
+                                            />
+                                        </S.Profile>
+                                        <S.Name>lovtaeha</S.Name>
+                                        <S.Time>2일</S.Time>
+                                    </div>
+                                    <S.AddButton>...</S.AddButton>
+                                </S.Header>
+                                <S.Photo>
+                                    <S.SwiperWrap {...SettingsFeed}>
+                                        <S.Slide>
+                                            {' '}
+                                            <img
+                                                src='https://picsum.photos/id/237/500/500'
+                                                alt=''
+                                            />
+                                        </S.Slide>
+                                        <S.Slide>
+                                            {' '}
+                                            <img
+                                                src='https://picsum.photos/id/237/500/500'
+                                                alt=''
+                                            />
+                                        </S.Slide>
+                                        <S.Slide>
+                                            {' '}
+                                            <img
+                                                src='https://picsum.photos/id/237/500/500'
+                                                alt=''
+                                            />
+                                        </S.Slide>
+                                        <S.Slide>
+                                            {' '}
+                                            <img
+                                                src='https://picsum.photos/id/237/500/500'
+                                                alt=''
+                                            />
+                                        </S.Slide>
+                                        <S.Slide>
+                                            {' '}
+                                            <img
+                                                src='https://picsum.photos/id/237/500/500'
+                                                alt=''
+                                            />
+                                        </S.Slide>
+                                        <S.Slide>
+                                            {' '}
+                                            <img
+                                                src='https://picsum.photos/id/237/500/500'
+                                                alt=''
+                                            />
+                                        </S.Slide>
+                                    </S.SwiperWrap>
+                                </S.Photo>
+                                <S.ActionIcon>
+                                    <div>
+                                        <S.Like>좋아요</S.Like>
+                                        <S.Reply>댓글</S.Reply>
+                                        <S.Share>공유</S.Share>
+                                    </div>
+                                    <S.BookMark>저장</S.BookMark>
+                                </S.ActionIcon>
+                                <S.LikePeople>
+                                    님 <span>여러 명</span>이 좋아합니다
+                                </S.LikePeople>
+                                <S.Post>
+                                    <S.Name>lovtaeha</S.Name>
+                                    <S.Text>
+                                        어린이 날 스스로 계획한대로...{' '}
+                                        <span>더보기</span>
+                                    </S.Text>
+                                </S.Post>
+                            </S.Feed>
+                        </S.FeedSection>
+                    </S.MainContents>
+                    {/* {!isMobileSize && <S.Recommend>recommend</S.Recommend>} */}
+                </S.Contents>
+            </S.Inner>
+        </S.Main>
+    );
 };
 
 export default MainFeed;
